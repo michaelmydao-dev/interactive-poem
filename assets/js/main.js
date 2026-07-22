95846ef0e1166ef0e1803bcaf7eb3b0be3938429
Michael Anh My Dao <michael.my.dao@gmail.com>
2026-07-22 01:37:47 +0000

    daw

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,38 @@
 // main.js
 
-// Define Random Number Function
+// Generate a random whole number
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Define divs variable as all divs inside grid container
-let divs = document.querySelectorAll(".grid-container div");
+// Generate a random decimal number
+function randomDecimal(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
-//Define Randomize function
+// Select all divs inside the grid container
+const divs = document.querySelectorAll(".grid-container div");
+
 function randomize() {
     console.log(divs.length, "randomize!");
 
-    //just the grid divs
-        divs.forEach( function (div) {
-           let scale = randomNumber(.5, 1.2);
-           let translateX = randomNumber( 0, 50)
-           let translateY = randomNumber( 0, 20)
-           let rotate = randomNumber(0, 360);
+    divs.forEach(function (div) {
+        let scale = randomDecimal(0.5, 1.2);
+        let translateX = randomNumber(0, 50);
+        let translateY = randomNumber(0, 20);
+        let rotate = randomNumber(0, 360);
 
-           // 50% of the time ...
-           if (Math.random() > .5) {
-            scale = 1; // dont affect scale
-            translateX = randomNumber (0,80);
-            translateY = randomNumber (0,80);
-           }  
+        // 50% of the time, do not change the scale
+        if (Math.random() > 0.5) {
+            scale = 1;
+            translateX = randomNumber(0, 80);
+            translateY = randomNumber(0, 80);
+        }
 
-            div.style.transform = `scale(${scale}) translate($
-            {translateX}%, ${translateY}%) rotate(${rotate}deg)`;
-
-     
-        })
+        div.style.transform =
+            `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
+    });
 }
-    
-// Add event listener to call randomizer function 
-document.addEventListener("click", randomize);
 
+// Animate when anywhere on the page is clicked
+document.addEventListener("click", randomize);
