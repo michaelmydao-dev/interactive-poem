219bc2811b9cb15d6aebaaa0d738e4388aa4cbb8
Michael Anh My Dao <michael.my.dao@gmail.com>
2026-07-22 01:14:09 +0000

    added java

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 // main.js
 
-console.log("Hello, World!");
+// Define Random Number Function
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Define Div variable as all divs inside grid container
+let divs = document.querySelectorAll(".grid-container div");
+
