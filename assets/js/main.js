4e239caf17da518b9db6bb09f9539e18e5b73195
Michael Anh My Dao <michael.my.dao@gmail.com>
2026-07-22 01:19:04 +0000

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,18 @@ function randomNumber(min, max) {
 // Define Div variable as all divs inside grid container
 let divs = document.querySelectorAll(".grid-container div");
 
+//Dfine Randomize function
+function randomize() {
+    console.log(divs.length, "randomiz!");
+
+    //just the grid divs
+        divs.forEach(function (div){
+           let scale = randomNumber(.5, 1.2);
+           let translateX = randomNumber( 0, 50)
+           let translateY = randomNumber( 0, 200)
+           let rotate = randomNumber(0, 360)
+        
+            
+        })
+}
+    
