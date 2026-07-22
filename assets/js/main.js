a965b091e40f6af19be3ea9a197d701c3a668551
Michael Anh My Dao <michael.my.dao@gmail.com>
2026-07-22 01:24:14 +0000

    added js div thingy

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,9 +17,18 @@ function randomize() {
            let scale = randomNumber(.5, 1.2);
            let translateX = randomNumber( 0, 50)
            let translateY = randomNumber( 0, 200)
-           let rotate = randomNumber(0, 360)
-        
-            
+           let rotate = randomNumber(0, 360);
+
+           // 50% of the time ...
+           if (Math.random() > .5) {
+            scale = 1; // dont affect scale
+            translateX = randomNumber (0,80);
+            translateY = randomNumber (0,80);
+           }  
+
+            div.style.transform = `scale(${scale}) translate($
+            {translateX}%${translateY}%) rotate(${rotate}deg)`;
+
         })
 }
     
